96057ae08940cc3d8e0c51ac62ba5b91dc0a5f65
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2016-09-04 00:27:44 +0400

    :ru: Добавлена поддержка vimeo

--- assets/plugins/videoembed/plugin.videoembed.php
+++ assets/plugins/videoembed/plugin.videoembed.php
@@ -20,6 +20,7 @@ if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 global $modx, $YtBetpl, $defYtStyle, $YtStyle, $icludeYtStyle;
 $YtBetpl = isset($tpl) ? $modx->getChunk($tpl) : null;
 $defYtStyle = $icludeYtStyle = 0;
+
 $YtStyle = '
 <style>
 .embed-responsive {
@@ -49,24 +50,25 @@ $YtStyle = '
 	padding-bottom: 75%;
 }
 </style>';
+
 $defaultTpl = '
 <div class="embed-responsive embed-responsive-16by9">
-	<iframe src="[+youtube+]" frameborder="0" allowfullscreen></iframe>
+	<iframe src="[+videoembed+]"  frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 </div>';
+
 $e =&$modx->event;
 
 if(empty($YtBetpl)){
 	$defYtStyle = 1;
 	$YtBetpl = $defaultTpl;
 }
-/*
-<iframe width="1366" height="668" src="https://www.youtube.com/embed/axd2JccV8MA?list=PLfpYsuT2dXbzcDEvD7e8qApvGNvtpf1Fq" frameborder="0" allowfullscreen></iframe>
-*/
-if(!function_exists("youtube_clear")) {
-	function youtube_clear(){
+
+if(!function_exists("videoembed_clear")) {
+	function videoembed_clear(){
 		return "<noindex><strong style=\"color:red;\">Invalid Video Embed</strong></noindex>";
 	}
 }
+
 if(!function_exists("youtube_replacer")) {
 	function youtube_replacer(&$matches){
 		global $modx, $YtBetpl, $defYtStyle, $YtStyle, $icludeYtStyle;
@@ -87,8 +89,6 @@ if(!function_exists("youtube_replacer")) {
 		4.	[122-156]	`PLfpYsuT2dXbzcDEvD7e8qApvGNvtpf1Fq`
 		5.	[164-174]	`{/youtube}`
 		**/
-		
-		
 		if(empty($matches[3])){
 			return $html;
 		}
@@ -97,7 +97,7 @@ if(!function_exists("youtube_replacer")) {
 		$list = (empty($matches[4])) ? "?rel=0" : "?list={$matches[4]}&rel=0";
 		$youtube = array("//www.youtube-nocookie.com/embed/", $matches[3], $list);
 		
-		$embed = array("youtube"=>implode("", $youtube));
+		$embed = array("videoembed"=>implode("", $youtube));
 		
 		$html .= $modx->parseText($YtBetpl, $embed, '[+', '+]');
 		
@@ -105,11 +105,46 @@ if(!function_exists("youtube_replacer")) {
 	}
 }
 
+if(!function_exists('vimeo_replacer')){
+	function vimeo_replacer(&$matches){
+		global $modx, $YtBetpl, $defYtStyle, $YtStyle, $icludeYtStyle;
+		$html = "";
+		/**
+		MATCH 1
+		1.	[0-42]	`{vimeo}https://vimeo.com/181035919{/vimeo}`
+		2.	[0-7]	`{vimeo}`
+		3.	[7-34]	`https://vimeo.com/181035919`
+		4.	[25-34]	`181035919`
+		5.	[34-42]	`{/vimeo}`
+		**/
+		if(empty($matches[4])){
+			return $html;
+		}
+		$html .= ($defYtStyle==1 && $icludeYtStyle==0) ? $YtStyle : "";
+		$icludeYtStyle = 1;
+		$embed = array("videoembed"=>"//player.vimeo.com/video/{$matches[4]}?title=0&byline=0&portrait=0&badge=0");
+		$html .= $modx->parseText($YtBetpl, $embed, '[+', '+]');
+		
+		return $html;
+	}
+}
 switch ($e->name) {
 	case "OnWebPagePrerender":{
 		$outputPrepare = $modx->documentOutput;
+		/*
+		-- YouTube
+		*/
 		$re = "@((\{youtube\})(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|watch\?v=|watch\?.+(?:&|&#38;|&amp;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;|&amp;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?(\{\/youtube\}))@i";
 		$outputPrepare = preg_replace_callback($re, 'youtube_replacer', $outputPrepare);
+		/**
+		*** Добавить поддержку других серверов видео
+		*** (ProjectSoft)
+		**/
+		/*
+		-- Vimeo
+		*/
+		$re = "@((\{vimeo\})((?:https?:\/\/)?(?:www\.)?(?:vimeo\.com\/)(.*))(\{\/vimeo\}))@i";
+		$outputPrepare = preg_replace_callback($re, 'vimeo_replacer', $outputPrepare);
 		/**
 		*** Добавить поддержку других серверов видео
 		*** (ProjectSoft)
@@ -118,8 +153,8 @@ switch ($e->name) {
 		/**
 		*** Очищаем
 		**/
-		$reclean = "@((\{youtube\})(.+)(\{\/youtube\}))@i";
-		$outputPrepare = preg_replace_callback($reclean, 'youtube_clear', $outputPrepare);
+		$reclean = "@((?:\{(?:youtube|vimeo)\})(?:.+)(?:\{\/(?:youtube|vimeo)\}))@i";
+		$outputPrepare = preg_replace_callback($reclean, 'videoembed_clear', $outputPrepare);
 		$modx->documentOutput = $outputPrepare;
 		break;
 	}

4ec974ef7aba40354ff55ddd490a4e1bb0c528ce
IOWEB <projectsoft@ioweb.ru>
2016-09-03 22:19:27 +0400

    :ru: Обновлено

--- assets/plugins/youtube/plugin.youtube.php
+++ assets/plugins/youtube/plugin.youtube.php
@@ -16,14 +16,61 @@
 */
 //author ProjectSoft (projectsoft@ioweb.ru)
 if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
-global $modx;
-$tpl = isset($tpl) ? $tpl : ;
+
+global $modx, $YtBetpl, $defYtStyle, $YtStyle, $icludeYtStyle;
+$YtBetpl = isset($tpl) ? $modx->getChunk($tpl) : null;
+$defYtStyle = $icludeYtStyle = 0;
+$YtStyle = '
+<style>
+.embed-responsive {
+	position: relative;
+	display: block;
+	height: 0;
+	padding: 0;
+	overflow: hidden;
+}
+.embed-responsive iframe,
+.embed-responsive embed,
+.embed-responsive object,
+.embed-responsive video {
+	position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    border: 0;
+}
+.embed-responsive-16by9 {
+	padding-bottom: 56.25%;
+}
+
+.embed-responsive-4by3 {
+	padding-bottom: 75%;
+}
+</style>';
+$defaultTpl = '
+<div class="embed-responsive embed-responsive-16by9">
+	<iframe src="[+youtube+]" frameborder="0" allowfullscreen></iframe>
+</div>';
 $e =&$modx->event;
 
+if(empty($YtBetpl)){
+	$defYtStyle = 1;
+	$YtBetpl = $defaultTpl;
+}
+/*
+<iframe width="1366" height="668" src="https://www.youtube.com/embed/axd2JccV8MA?list=PLfpYsuT2dXbzcDEvD7e8qApvGNvtpf1Fq" frameborder="0" allowfullscreen></iframe>
+*/
+if(!function_exists("youtube_clear")) {
+	function youtube_clear(){
+		return "<noindex><strong style=\"color:red;\">Invalid Video Embed</strong></noindex>";
+	}
+}
 if(!function_exists("youtube_replacer")) {
 	function youtube_replacer(&$matches){
-		global $modx;
-		$html = print_r($matches, true);
+		global $modx, $YtBetpl, $defYtStyle, $YtStyle, $icludeYtStyle;
+		$html = "";
 		/**
 		MATCH 1
 		1.	[0-62]	`{youtube}https://www.youtube.com/watch?v=E_cb5Dc5vbA{/youtube}`
@@ -42,27 +89,30 @@ if(!function_exists("youtube_replacer")) {
 		5.	[164-174]	`{/youtube}`
 		**/
 		
-		$id = $matches[3];
-		$list = $matches[4];
-		if(empty($id)){
+		
+		if(empty($matches[3])){
 			return $html;
 		}
+		$html = ($defYtStyle==1 && $icludeYtStyle==0) ? $YtStyle : "";
+		$icludeYtStyle = 1;
+		$list = (empty($matches[4])) ? "?rel=0" : "?list={$matches[4]}&rel=0";
+		$youtube = array("//www.youtube-nocookie.com/embed/", $matches[3], $list);
+		
+		$embed = array("youtube"=>implode("", $youtube));
+		
+		$html .= $modx->parseText($YtBetpl, $embed, '[+', '+]');
+		
 		return $html;
 	}
 }
-if(!function_exists("youtube_parsetext")) {
-	function youtube_parsetext(){
-		global $modx;
-		$embed = "https://www.youtube-nocookie.com/embed/%id%%list%";
-		$out = "";
-		return $out;
-	}
-}
+
 switch ($e->name) {
 	case "OnWebPagePrerender":{
 		$outputPrepare = $modx->documentOutput;
-		$re = "@((\{youtube\})(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|watch\?v=|watch\?.+(?:&|&#38;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?(\{\/youtube\}))@i";
+		$re = "@((\{youtube\})(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|watch\?v=|watch\?.+(?:&|&#38;|&amp;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;|&amp;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?(\{\/youtube\}))@i";
 		$outputPrepare = preg_replace_callback($re, 'youtube_replacer', $outputPrepare);
+		$reclean = "@((\{youtube\})(.+)(\{\/youtube\}))@i";
+		$outputPrepare = preg_replace_callback($reclean, 'youtube_clear', $outputPrepare);
 		$modx->documentOutput = $outputPrepare;
 		break;
 	}
